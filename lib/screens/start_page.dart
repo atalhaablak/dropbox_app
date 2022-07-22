4a6ea6ce72cdd93ad5e82d44fa1199bafff33b01
atalhaablak <76072113+atalhaablak@users.noreply.github.com>
2022-07-22 23:18:07 +0300

    Created Start Page

---
+++ lib/screens/start_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
